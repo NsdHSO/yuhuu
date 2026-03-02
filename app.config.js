abc36202580b3eda7fbace3b9e3c3ed71133b7a4
NsdHSO <nechiforelsamuel@gmail.com>
2026-03-02 18:12:39 +0200

    chore: 🌻 update app config for i18n support

--- app.config.js
+++ app.config.js
@@ -38,6 +38,7 @@ module.exports = {
         plugins: [
             'expo-router',
             'expo-secure-store',
+            'expo-localization',
             [
                 'expo-local-authentication',
                 {
