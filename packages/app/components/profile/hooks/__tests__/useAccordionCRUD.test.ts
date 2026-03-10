4863d5e92e196c8a5fd621e39fde4c0d8b20cbda
NsdHSO <nechiforelsamuel@gmail.com>
2026-03-10 09:51:00 +0200

    fix: 🛠️ remove unused waitFor import in useAccordionCRUD test

--- packages/app/components/profile/hooks/__tests__/useAccordionCRUD.test.ts
+++ packages/app/components/profile/hooks/__tests__/useAccordionCRUD.test.ts
@@ -1,4 +1,4 @@
-import {renderHook, act, waitFor} from '@testing-library/react-native';
+import {renderHook, act} from '@testing-library/react-native';
 import {Alert} from 'react-native';
 import {useAccordionCRUD} from '../useAccordionCRUD';
 
